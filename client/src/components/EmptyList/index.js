eacb3eff536c7ad6a02f864e2a568ea87233bb1c
carlitos <cmmurillo1@hotmail.com>
2019-10-25 14:11:54 -0400

    homework

---
+++ client/src/components/EmptyList/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css"
+
+export default function EmptyList(){
+    return(
+        <div>
+            <h2>No Books To Display, Let's search!!!</h2>
+        </div>
+    )
+}
